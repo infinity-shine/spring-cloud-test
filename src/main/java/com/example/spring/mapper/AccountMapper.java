bcb8d7ee19aaa604b9a73c23264d38aac37c70ac
YangWenQi <1908365891@qq.com>
2022-06-11 15:14:48 +0800

    第一次提交

---
+++ src/main/java/com/example/spring/mapper/AccountMapper.java
@@ -0,0 +1,14 @@
+package com.example.spring.mapper;
+
+/**
+ * @ author yangwenguang
+ * @ date 2022/1/12
+ */
+public class AccountMapper {
+
+    public void saveAccount() {
+        System.out.println("Save Account");
+
+    }
+
+}
